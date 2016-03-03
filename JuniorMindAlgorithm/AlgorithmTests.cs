03c93855ab02e7ed4d92b35839e35fccb5575225
alinaoarga <oarga_alina@yahoo.com>
2016-03-03 11:59:52 +0200

    ConvertDecimalToBinary Function

---
+++ JuniorMindAlgorithm/AlgorithmTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JuniorMindAlgorithm
+{
+    [TestClass]
+    public class AlgorithmTests
+    {
+        [TestMethod]
+        public void ConvertNumber22()
+        {
+            Assert.AreEqual("10110", ConvertDecimalToBinary(22));
+        }
+        [TestMethod]
+        public void ConvertNumber13()
+        {
+            Assert.AreEqual("1101", ConvertDecimalToBinary(13));
+        }
+        string ConvertDecimalToBinary(int number)
+        {
+            string result = string.Empty;
+            while (number > 0)
+            {
+                int remainder = number % 2;
+                number /= 2;
+                result = remainder.ToString() + result;
+            }
+            return result;
+        }
+    }
+}

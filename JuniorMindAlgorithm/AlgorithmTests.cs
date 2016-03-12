d372118847c4afd9375036a55c549810990dfc17
alinaoarga <oarga_alina@yahoo.com>
2016-03-12 13:29:33 +0200

    Algorithm Tests

--- JuniorMindAlgorithm/AlgorithmTests.cs
+++ JuniorMindAlgorithm/AlgorithmTests.cs
@@ -16,20 +16,26 @@ namespace JuniorMindAlgorithm
         {
             CollectionAssert.AreEqual(new byte [] { 1, 1, 0, 1}, ConvertDecimalToBinary(13));
         }
+        [TestMethod]
+        public void ConvertNumber11()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 1, 1 }, ConvertDecimalToBinary(11));
+        }
         byte [] ConvertDecimalToBinary(int number)
         {
             byte[] results = new byte[0];
-            Array.Resize(ref results, 4);
+            int index = results.Length + 1;
+           
             while (number > 0)
             {
                 int remainder = (number % 2);
                 number /= 2;
-                for (int i = 0; i <= results.Length; i++)
-                {
-                    results[i] += Convert.ToByte(remainder);
-                }
+                Array.Resize(ref results, index);
+                results[index - 1] = Convert.ToByte(remainder);
+                index++; 
             }
-            return results ;
+            Array.Reverse(results);
+            return results;
         }
     }
 }

b80a110f8e41745e8f801785c629fa1c9c8b76ce
alinaoarga <oarga_alina@yahoo.com>
2016-03-15 10:59:44 +0200

    Not11 Failed Test

--- JuniorMindAlgorithm/AlgorithmTests.cs
+++ JuniorMindAlgorithm/AlgorithmTests.cs
@@ -24,7 +24,7 @@ namespace JuniorMindAlgorithm
         [TestMethod]
         public void Not11()
         {
-            CollectionAssert.AreEqual(new byte[] { 0, 0, 0, 0}, NotOperand(new byte[] { 1, 0, 1, 1 }));
+            CollectionAssert.AreEqual(new byte[] { 0, 0, 0, 0 }, NotOperand(new byte[] { 1, 0, 1, 1 }));
         }
         byte[] ConvertDecimalToBinary(int number)
         {
@@ -39,15 +39,14 @@ namespace JuniorMindAlgorithm
             Array.Reverse(results);
             return results;
         }
-        byte [] NotOperand(byte [] number)
+        byte[] NotOperand(byte[] number)
         {
-            foreach (byte element in number)
+            byte i = 0;
+            byte element = 1;
+            while (number[i] == 1) 
             {
-                if(element == 1)
-                {
-                    byte newElement = 0;
-                    number[element] = number[newElement];
-                }
+                number[i] -= element;
+                i++;
             }
             return number;
         }
